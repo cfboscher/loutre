fea77fa8aca577b607eea417af08509279dd3000
Nicolas Nadisic <nicolas@nadisic.tk>
2015-04-29 17:02:46 +0200

    Inclusion complète des requetes BD dans le script Python (non testé sur rpi)

--- scriptsRPi/dossier_courant/principal.py
+++ scriptsRPi/dossier_courant/principal.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 #LOUTRE
 #Script principal
 #Copyright : LOUTRE & Contributeurs, 2015.
 #Licence GNU GPL v3
+
+### VERSION PAS TESTEE SUR LE RPI ###
 
 ########################################################################
 ### IMPORT
@@ -15,22 +17,23 @@ import datetime
 from time import sleep
 
 import nxppy
-from Adafruit_CharLCD import Adafruit_CharLCD
+#from Adafruit_CharLCD import Adafruit_CharLCD
+
+import MySQLdb
 
 
 ########################################################################
 ### CONSTANTES
 ########################################################################
 
-#Prix d'une consommation
-prixUnitaire = 42
-
 #Temps d'attente lors de la lecture, en secondes
 #(première lecture : affichage du solde, puis temporisation)
 #(deuixème lecture : validation de la transaction)
 #(si pas de deuxième lecture avant tempoLect2, annulation)
+#tempoPrelev : temps de la tempo après validation de transaction
 tempoLect1 = 1
 tempoLect2 = 5
+tempoPrelev = 1
 unitePaiement=" groseilles "
 
 
@@ -39,11 +42,7 @@ unitePaiement=" groseilles "
 ########################################################################
 
 #Initialisation de l'écran LCD
-lcd=Adafruit_CharLCD()
-lcd.begin(16, 1)
-
-#Ouverture du lecteur NFC
-mifare=nxppy.Mifare()
+#lcd.begin(16, 1)
 
 
 ########################################################################
@@ -51,6 +50,9 @@ mifare=nxppy.Mifare()
 ########################################################################
 
 #Ecriture parallèle dans le terminal et dans le fichier de journal
+#Usage : lprint(chaine) sans mettre de \n dans chaine ou après : 
+# le \n est inclus automatiquement dans l'écriture dans les log
+# et est inutile pour l'affichage dans un terminal avec print 
 def lprint (chaine):
 	print chaine
 	#Ouverture initiale du fichier de journal
@@ -66,47 +68,77 @@ def lprint (chaine):
 ### BOUCLE PRINCIPALE
 ########################################################################
 
+# Se connecter à la BD
+db = MySQLdb.connect(host="localhost", user="python", passwd="jambeo", db="Loutre")
+
+
+
 while 1:
-    while 1:
-        try:
-            uid = mifare.select()
-            lprint("Read UID : " + str(uid))
-            ### Insérer appel procédure mySQL pour l'affichage ###
-            argentRestant = 500 #Remplacer par le montant de la fin de dos
-            lcd.clear()
-            lcd.message("Argent restant : \n")
-            lprint("Argent restant : \n")
-            lcd.message(str(argentRestant) + nomMonnaieyes)
-            lprint(str(argentRestant) + nomMonnaie)
-            sleep(tempoLect1)
-            
-            instantLecture1=datetime.datetime.utcnow()
-            
-            a = True
-            while a:
-				#Après expiration de la tempo la transaction est annulée
-				now=datetime.datetime.now()
-				if (now - instantLecture1) > timedelta(seconds = tempoLect2):
-					break
+	try:
+		#Ouverture du lecteur NFC
+		mifare=nxppy.Mifare()
+		uid = mifare.select()
+		lprint("Read UID : " + str(uid))
+		
+		##### Requete sur BD pour consulter le solde #####
+		commande="SELECT Solde FROM Personne WHERE ID="
+		commande+=str(uid)
+		c = self.db.cursor()
+		c.execute(commande)
+		rows = c.fetchall() #rows va contenir tout les résultats
+		c.close()
+		# Traitement des résultats de notre requête
+		solde=row[0]
+		for row in rows:
+			solde=row[0]
+		##### Fin de requete #####
+		
 				
-				try:
-					uid2 = mifare.select()
-					lprint ("Read UID : " + str(uid2))
+		argentRestant = solde #Remplacer par le montant du backend
+		lcd.clear()
+		lcd.message("Argent restant : \n")
+		lprint("Argent restant : ")
+		lcd.message(str(argentRestant) + unitePaiement)
+		lprint(str(argentRestant) + unitePaiement)
+		sleep(tempoLect1)
+		
+		instantLecture1=datetime.datetime.utcnow()
+		
+		a = True
+		while a:
+            #Après expiration de la tempo la transaction est annulée
+			now=datetime.datetime.now()
+			if (now - instantLecture1) > datetime.timedelta(seconds = tempoLect2):
+				break
+			
+			try:
+				uid2 = mifare.select()
+				lprint ("Read UID : " + str(uid2))
 
-					if (uid2==uid):
-						a = false
-						### Insérer appel procédure MySQL (mon langage de requete structuré )  pour le prélèvement ###
-						lcd.clear()
-						lcd.message(str(prixUnitaire)+unitePaiement+"\n")
-						lcd.message("ont été prélevées.")
-						lprint(str(prixUnitaire)+unitePaiement+"ont été prélevées")
-						
-                #Tant que on ne lit pas de carte, ne rien faire et recommencer
-				except nxppy.SelectError:
-					pass
+				if (uid2==uid):
+					a = False
+					
+					##### Requete SQL pour le prélèvement #####
+					commande="INSERT INTO (ID_Etudiant,Montant) VALUES ("
+					commande+=str(uid)
+					commande+=", SELECT - prix_boisson FROM parametres"
+					c = self.db.cursor()
+					c.execute(commande)
+					##### Fin de requete #####
+					
+					lcd.clear()
+					lcd.message("Moult"+unitePaiement+"\n")
+					lcd.message("ont été prélevées.")
+					lprint("Moult"+unitePaiement+"ont été prélevées")
+					sleep(tempoPrelev)
+					
+			#Tant que on ne lit pas de carte, ne rien faire et recommencer
+			except nxppy.SelectError:
+				pass
+	
+	#Tant que on ne lit pas de carte, ne rien faire et recommencer
+	except nxppy.SelectError:
+		pass
 
-        #Tant que on ne lit pas de carte, ne rien faire et recommencer
-        except nxppy.SelectError:
-            pass
 
 
