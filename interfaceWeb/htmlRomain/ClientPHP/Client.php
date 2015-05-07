059f2f74e891b58a86f37ae5eb32314061c10346
Mookinete <marie.leguilly@gmail.com>
2015-05-07 15:26:00 +0200

    PHP avancé

---
+++ interfaceWeb/htmlRomain/ClientPHP/Client.php
@@ -0,0 +1,106 @@
+<?php
+	$name = $_POST["prenom"];
+	$IDClient = $_POST["IDClient"];
+	
+	$db = mysql_connect('localhost', 'login', 'password');
+	mysql_select_db('nomdelabase', $db);
+	$sqlSolde = "SELECT Solde FROM Personne WHERE ID=IDClient";
+	$reqSolde = mysql_query($sqlConnect);
+	
+	$solde = $reqSolde->fetch();
+
+?>
+
+<hmtl>
+	<head>
+		<title>LOUTRE</title>
+		<link rel="stylesheet" href="../Ressources/styleLoutre.css" />
+	</head>
+
+	<body >
+		<div id="logo">
+		
+			<a href="../Connexion.html"><img src="../Ressources/LogoLoutre.jpg" alt="Logo Loutre" style="width:800px"></a>
+		</div>
+
+		<div id="page-wrap">
+			<h1>Bonjour  <a href="<?php print $name; ?>"></a>, heureux de vous revoir!</h1>
+			<h2>Il vous reste  <a href="<?php print $solde; ?>"></a>€ sur votre compte.</h2>
+		</div>
+
+		<div id="page-wrap">
+			<h2>Détails de vos mouvements de compte : </h2>
+			<table id="conso">
+				<tr>
+					<th>Date</th>
+					<th>Opération</th> 
+					<th>Mouvement</th>
+				</tr>
+				<tr>
+					<td>11/03/2015</td>
+					<td>Consommation : café</td> 
+					<td>-00,50€</td>
+				</tr>
+				<tr>
+					<td>09/03/2015</td>
+					<td>Virement vers : Michel</td> 
+					<td>-51,00€</td>
+				</tr>
+				<tr>
+					<td>06/03/2015</td>
+					<td>Virement depuis : Jacquie</td> 
+					<td>+24,00€</td>
+				</tr>
+				<tr>
+					<td>24/02/2015</td>
+					<td>Renflouement des caisses</td> 
+					<td>+12,00€</td>
+				</tr>	
+				<tr>
+					<td>17/02/2015</td>
+					<td>Paiement : Sortie Ski</td> 
+					<td>-54,00€</td>
+				</tr>
+				<tr>
+					<td>16/02/2015</td>
+					<td>Cadeau de bienvenue</td> 
+					<td>+01,00€</td>
+				</tr>
+			</table>
+		</div>
+		
+		<div id="page-wrap">
+		<h2> Virement vers un autre utilisateur : </h2>
+		<br>
+				Envoi de
+				<input type="text" name="montant">
+				€ à
+				<select>
+					<option value ="sydney">Jacquie</option>
+					<option value ="melbourne">Michel</option>
+					<option value ="cromwell">Robert</option>
+					<option value ="queenstown">Roger</option>
+				</select>
+				?
+				<button> Valider le virement </button>
+		</div>
+		
+		<div id="page-wrap">
+			<h2> Inscription aux évènements AEDI: </h2>
+			<br>
+			Je souhaite m'inscrire à l'événement : 
+			<select>
+				<option value ="sydney">WEI (25€)</option>
+				<option value ="melbourne">Sortie SKI (52€)</option>
+				<option value ="cromwell">Bowling (4€)</option>
+				<option value ="queenstown">Hackathon Google (gratuit)</option>
+			</select>
+				
+			<button> S'inscrire </button>
+			<br>
+			Commentaire éventuel :
+			<input type="text" name="montant">
+		</div>
+
+	</body>
+</html>
--- interfaceWeb/htmlRomain/Connexion.php
+++ interfaceWeb/htmlRomain/Connexion.php
@@ -1,32 +1,4 @@
-<hmtl>
-	<head>
-		<title>LOUTRE</title>
-		<link rel="stylesheet" href="./Ressources/styleLoutre.css" />
-	</head>
-
-	<body >
-		<div id="logo">
-			<img src="./Ressources/LogoLoutre.jpg" alt="Logo Loutre" style="width:800px">
-		</div>
-
-		<div id="connexion">
-			<h1>Bienvenue sur L.O.U.T.R.E.!</h1>
-			<h2>Loutre est un Outil Unifié pour des Transactions Régulières Efficaces</h2>
-		</div>
-
-		<div id="connexion">
-			<h2>Veuillez s'il vous plait vous connecter : </h2>
-			nom:
-			<br><input type="text" name="username">
-			<br>
-			prenom :
-			<br><input type="text" name = "prenom">
-			<br>
-			mot de passe:
-			<br><input type="password" name="pass">
-			<br>
-			
-			<?php
+		<?php
 			//On récupère les données entrées dans les champs
 			$nom = $_POST['username'];
 			$prenom = $_POST['prenom'];
@@ -58,13 +30,27 @@
 				$sqlConnect = 'SELECT *FROM Personne WHERE UCASE(SUBSTR(Personne.Prenom, 0, 1)) = UCASE(SUBSTR($prenom,0,1))AND UCASE(SUBSTR(Personne.Nom,1)) = UCASE(SUBSTR($nom,1)) AND Personne.Password=$mdp';
 				$reqConnect = mysql_query($sqlConnect);
 			}
-			?>
 			
-			<button> Connexion </button>
-		</div>
-			<h3>Accès rapide (phase de tests):</h3>
-			<a href="./Client/Client.html"><button> Client </button></a><br>
-			<a href="./Admin/Accueil.html"><button> Admin </button></a>
-		</div>		
-	</body>
-</html>
+			$answer = $reqConnect->fetch();
+			$role = $answer['Role'];
+			$id = $answer['ID'];
+		?>
+			
+		<html>
+			if($role == 1)
+			{
+				<form method=POST action="/AdminPHP/Accueil.php">
+				   <input type="hidden" name="IDAdmin" value=".$ID."></input>
+				</form>
+			}
+			
+			else
+			{
+			<form method=POST action="/ClientPHP/Client.php">
+			   <input type="hidden" name="IDClient" value=".$ID."></input>
+			   <input type="hidden" name="PrenomClient" value=".$prenom."></input>
+
+			</form>
+			}
+		</html>	
+			
