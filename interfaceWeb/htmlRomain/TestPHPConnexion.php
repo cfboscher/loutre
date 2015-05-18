b9d8c95fbf7607afa7d5f07618bbb26eb0d9ab20
Mookinete <marie.leguilly@gmail.com>
2015-05-18 19:10:11 +0200

    Test php 24

--- interfaceWeb/htmlRomain/TestPHPConnexion.php
+++ interfaceWeb/htmlRomain/TestPHPConnexion.php
@@ -9,7 +9,6 @@
 			echo "Nom entré : ".$_POST[username];
 			
 			echo '<form method="post" action="/marie/Client/Client.html">
-					<input type="hidden" name="nom" value=$_POST[username]>
 				 </form>';
 				 
 			header('Location: /marie/Client/Client.html');      
