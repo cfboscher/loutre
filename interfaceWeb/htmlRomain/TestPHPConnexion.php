43c9ca87e1aaf96b253d9651931734175766c4ab
Mookinete <marie.leguilly@gmail.com>
2015-05-20 12:28:50 +0200

    Test php 46

--- interfaceWeb/htmlRomain/TestPHPConnexion.php
+++ interfaceWeb/htmlRomain/TestPHPConnexion.php
@@ -9,12 +9,15 @@
 			echo "Nom d'utilisateur : ".$_POST[username];
 			
 			$nom = $_POST[username];
+			$pass = $_POST[pass];
 	?>
 		<br>
 		Si la page ne se charge pas au bout de 5 secondes, cliquez ici : <br>
 		<form method="post" action="/marie/Client/Client.php">
 
-			<p><input type="hidden" name="coucou" value="<?php echo $nom;?>"/></p>
+			<p><input type="hidden" name="name" value="<?php echo $nom;?>"/></p>
+			<p><input type="hidden" name="value" value="<?php echo $pass;?>"/></p>
+
 			<input type="submit" value="Continuer">
 
 
