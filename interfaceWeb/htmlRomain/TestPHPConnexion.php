43654ea113d608fa547af6b55c388f99b4eb7881
Mookinete <marie.leguilly@gmail.com>
2015-05-21 14:48:45 +0200

    Test php 51

--- interfaceWeb/htmlRomain/TestPHPConnexion.php
+++ interfaceWeb/htmlRomain/TestPHPConnexion.php
@@ -11,29 +11,10 @@
 			$nom = $_POST[username];
 			$pass = $_POST[pass];
 			
-		// on se connecte à MySQL
-		$db = mysql_connect('localhost', 'phpuser', 'lambda');
-
-		// on sélectionne la base
-		mysql_select_db('Loutre',$db);
-
-		// on crée la requête SQL
-		$sql = 'SELECT * FROM Personne WHERE UCASE(SUBSTR(Personne.Prenom, 0, 1)) = UCASE(SUBSTR($username,0,1)) AND UCASE(SUBSTR(Personne.Nom,1)) = UCASE(SUBSTR(username,1))';
-
-		// on envoie la requête
-		$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-
-		// on fait une boucle qui va faire un tour pour chaque enregistrement
-		while($data = mysql_fetch_assoc($req))
+			if($nom == 'admin')
 			{
-				// on affiche les informations de l'enregistrement en cours
-				echo '<b>'.$data['nom'].' '.$data['prenom'].'</b> ('.$data['statut'].')';
-				echo ' <i>date de naissance : '.$data['date'].'</i><br>';
-				$identifiant = .$data[ID];
+				header('Location: /marie/Admin/Accueil.html');
 			}
-
-		// on ferme la connexion à mysql
-		mysql_close();
 	?>
 	
 		<br>
